1971eb6f6a2d32f8760ea0e1bbf9f3a207f208e2
Raja Ganapathi <rajacse1@gmail.com>
2024-08-07 13:07:04 +0530

    Commented authorization related code to execute

--- CommonSystem2-API/Controllers/BaseController.cs
+++ CommonSystem2-API/Controllers/BaseController.cs
@@ -4,8 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CommonSystem2_API.Controllers
 {
-    [Authorize(AuthenticationSchemes = "CredentialBasedAuth")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    //[Authorize(AuthenticationSchemes = "CredentialBasedAuth")]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     [ApiController]
     public class BaseController : ControllerBase
